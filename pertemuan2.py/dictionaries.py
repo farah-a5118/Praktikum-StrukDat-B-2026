0345cf98caef598dd1533d79da98b608994a8071
F.A <farah.aprelia4046@student.unri.ac.id>
2026-02-07 12:49:06 +0700

    dictionaries.py

--- pertemuan2.py/dictionaries.py
+++ pertemuan2.py/dictionaries.py
@@ -5,14 +5,14 @@ thisdict = {
   "model": "Mustang",
   "year": 1964
 }
-print(thisdict)
+print(thisdict) #Mencetak semua value yang ada di dictionary
 
 thisdict = {
   "brand": "Ford",
   "model": "Mustang",
   "year": 1964
 }
-print(thisdict["brand"])
+print(thisdict["brand"]) #Mencetak value dari variabel brand
 
 thisdict = {
   "brand": "Ford",
@@ -20,18 +20,18 @@ thisdict = {
   "year": 1964,
   "year": 2020
 }
-print(thisdict)
-print(len(thisdict))
+print(thisdict) #Mencetak semua value dalam dictionary
+print(len(thisdict)) #Menghitung berapa jumlah item yang ada di dalam dictionary
 
 thisdict = {
   "brand": "Ford",
   "model": "Mustang",
   "year": 1964
 }
-print(type(thisdict))
+print(type(thisdict)) #Mengecek tipe data, yang bertipe dict
 
-thisdict = dict(name = "John", age = 36, country = "Norway")
-print(thisdict)
+thisdict = dict(name = "John", age = 36, country = "Norway") #Ini adalah versi pendek dari dictionary
+print(thisdict) #Mencetak semua value dalam dictionary
 
 car = {
 "brand": "Ford",
@@ -39,8 +39,10 @@ car = {
 "year": 1964
 }
 
-x = car.values()
+x = car.values() #Untuk mengecek value yang ada di dalam dictionary, dan akan mengalami perubahan jika item di dalam dict diubah
 
-print(x) #before the change
+print(x)
 
-car["year"] = 2020
+car["year"] = 2020 #Value dalam variabel year akan berubah menjadi 2020
+
+print(x)
