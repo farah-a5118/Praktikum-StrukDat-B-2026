e13e1a877962675aa9278ac6875d4ff2f7637987
F.A <farah.aprelia4046@student.unri.ac.id>
2026-02-10 22:07:27 +0700

    pertemuan2.py directory

--- pertemuan2.py/latihan_pertemuan2.py/soal1.py
+++
@@ -1,6 +0,0 @@
-angka = [10, 20, 30, 40, 50]
-
-angka.insert(4, 60) #Menambah angka 60 pada indeks ke 4
-angka.remove(20) #Menghapus angka 20
-angka.reverse() #Mengurutkan angka dari tinggi ke rendah
-print(angka)
--- pertemuan2.py/latihan_pertemuan2.py/soal2.py
+++
@@ -1,7 +0,0 @@
-mahasiswa = ("A001", "Budi", "Informatika")
-
-print(mahasiswa[1])
-for x in mahasiswa:
-    print(x)
-
-#Tuple tidak dapat diubah karena memiliki urutan yang terorder. Namun tuple membolehkan elemen duplikat
